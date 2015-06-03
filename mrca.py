87cad391f6f29b8678bee274cf435ebab1a1e670
Kevin Heath <kheath@g.hmc.edu>
2015-06-03 13:19:53 -0700

    Adding file for mrca method

---
+++ mrca.py
@@ -0,0 +1,14 @@
+"""
+  Author: Kevin Heath
+  Date: 29 May 2015
+  Generated for summer research in the Bush lab 
+
+  Most Recent Common Ancestor
+"""
+
+def mrca(tree, family):
+    """ Identifies the most recent common ancestor of a gene family"
+    
+
+
+
