bb32cc55b76003339ca2d47ead783969c843ffc4
Kevin Heath <kheath@g.hmc.edu>
2015-06-08 16:13:59 -0700

    Made mrca work with new family structure.  Returns a tuple now though.  Probably still needs some work.

--- mrca.py
+++ mrca.py
@@ -9,14 +9,23 @@
   Example: python mrca.py treeFile
 """
 
-import copy, ast, sys
+import copy, ast, sys, argparse
 
 def main(argv):
 
+    # parser = argparse.ArgumentParser()
+    # requiredNamed = parser.add_argument_group('required named arguments')
+    # requiredNamed.add_argument('-t', '--tree', help='Tree file name ()', required=True)
+    # requiredNamed.add_argument('-f', '--family', help='family file name', required=True)
+    # requiredNamed.add_argument('-o', '--output', help='Output file name', required=True)
+    # requiredNamed.add_argument('-n', help='Number of species', type=int, required=True)
+    # requiredNamed.add_argument('-p', help='Partition file name', required=True)  
+    # parser.add_argument('-v', '--verbose', help='Increase output verbosity', action='store_true')
+    # args = parser.parse_args()
 
-    tree = readTree(argv[0])
+    tree = readTree('testAtree')
     
-    family = (1, -1, 1, 1, -1, -1, 0, 0, 0, 0, 0)
+    family = (3, 0, 1, 0, 0, 0)
 
     print mrca(tree, family)
 
@@ -41,31 +50,33 @@ def mrca(tree, family):
             return n
 
     """
-    
+    if max(family) <= 0:
+        return -1
+
     if tree[1] == () and tree[2] == ():
-      # print 'Found tip #'+str(tree[0])+'\t'+str([family[tree[0]], tree[0]])
-      if family[tree[0]] == -1:
-        return -1
-      else:
-        return tree[0]
+        # print 'Found tip #'+str(tree[0])+'\t'+str([tree[0], family[tree[0]]])
+        if family[tree[0]] == 0:
+            return (tree[0], 0)
+        else:
+            return (tree[0], 1)
     else:
-      r1 = mrca(tree[1], family)
-      r2 = mrca(tree[2], family)
+        r1 = mrca(tree[1], family)
+        r2 = mrca(tree[2], family)
 
-      # print 'Comparing r1 = '+str(r1)+' and r2 = '+str(r2)
+        # print 'Comparing r1 = '+str(r1)+' and r2 = '+str(r2)
 
-      if r1 == -1 and r2 == -1:
-        # print 'Returning '+str(-1)
-        return -1
-      elif r1 >= 0 and r2 >= 0:
-        # print 'Returning '+str(tree[0])
-        return tree[0]
-      elif r1 >= 0:
-        # print 'Returning '+str(r1)
-        return r1
-      else:
-        # print 'Returning '+str(r2)
-        return r2
+        if r1[1] == 0 and r2[1] == 0:
+            # print 'Returning '+str(-1)
+            return (tree[0],0)
+        elif r1[1] > 0 and r2[1] > 0:
+            # print 'Returning '+str(tree[0])
+            return (tree[0], 1)
+        elif r1[1] > 0:
+            # print 'Returning '+str(r1)
+            return r1
+        else:
+            # print 'Returning '+str(r2)
+            return r2
 
 
 
