75fc8914e2c51c6767769337b2d242e03b0a8d6c
Kevin Heath <kheath@g.hmc.edu>
2015-06-08 16:28:02 -0700

    First pass at making a class for groups of gene families

---
+++ group.py
@@ -0,0 +1,35 @@
+"""
+    Author: Kevin Heath
+    Date: 8 June 2015
+    Generated for summer research in the Bush lab 
+
+    Group class
+    'Holds gene families and their point of origin'
+
+    'I like trains'
+"""
+
+class Group:
+
+    def __init__(self, node, families):
+        self.origin = node
+        self.families = families
+
+    def size(self):
+        return len(self.families)
+
+    def origin(self):
+        return self.origin
+
+    def families(self):
+        return self.families
+
+    def addFamily(self, family):
+        if isinstance(family, list):
+            self.families.append(family)
+
+    def setOrigin(self, node):
+        self.origin = node
+
+
+
